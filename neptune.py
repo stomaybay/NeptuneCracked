3960be52e0bab0877ba6c885e87f08a27989c759
Exponent <103051123+Rdmo1@users.noreply.github.com>
2023-02-21 13:34:58 +0800

    Update neptune.py

--- neptune.py
+++ neptune.py
@@ -1,1 +1,3 @@
-DEMO FILE ONLY ( DO NOT DOWNLOAD )
+from colorama import Fore
+
+input(f'''{Fore.GREEN}are you dumb i said it is DEMO file only! you can buy the key from neptunekey.sellix.io and dm me on discord to get real file'''}
